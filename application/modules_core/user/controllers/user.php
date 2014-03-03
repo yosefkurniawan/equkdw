d4a37b6b941d540e97c582fa4e29821a66802516
yosefkurniawan <jojo.maniack@gmail.com>
2014-03-03 21:13:56 +0700

    adding login feature

--- application/modules_core/user/controllers/user.php
+++ application/modules_core/user/controllers/user.php
@@ -3,8 +3,53 @@
 class User extends MX_Controller
 {
 	
-	function index(){
-		echo "test page";
+	public function index(){
+		# checking whether logged in or not
+		(!isset($this->session->userdata['username'] )) ? redirect('user/login') : '';
+
+		echo "you're logged in!";
+	}
+
+	public function login(){
+
+		if(!empty($_POST)){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+
+			/* -- login to SSAT -- */
+			$curl = curl_init();
+			
+			curl_setopt_array($curl, array(
+				CURLOPT_RETURNTRANSFER 	=> 1,
+				CURLOPT_URL 			=> 'http://www.ukdw.ac.id/services/user/check_login',
+				CURLOPT_POST 			=> 1,
+				CURLOPT_POSTFIELDS 		=> array(
+					'api_key' 	=> 'EducationIsTheMovementFromDarknessToLight4ee0688948828187623123',
+					'id' 		=> $username,
+					'password' 	=> $password
+				)
+			));
+
+			$result = json_decode(curl_exec($curl), TRUE);
+			curl_close($curl);
+			/* -- /login to SSAT -- */
+			
+			if ($result['result']) {
+				$this->session->set_userdata('username', $result['id_user']);
+				$this->session->set_userdata('status', $result['status']);
+				if ($result['status'] = 'Mahasiswa') {
+					redirect('user/index');
+				}
+			}
+		}
+
+		$data['form_action'] = 'user/login';
+		$this->load->view('v_login',$data);
+	}
+
+	public function logout(){
+		$this->session->sess_destroy();
+		redirect('user/login');
 	}
 }
 
---
+++ application/modules_core/user/views/v_login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<form action="<?php echo base_url().$form_action ?>" method="POST">
+		<input type="text" name ="username"/>
+		<input type="password" name ="password"/>
+		<input type="submit" value="Login">
+	</form>
+
+</body>
+</html>
