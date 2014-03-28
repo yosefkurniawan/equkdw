7c5511a9bef422bf6f4c1f846ebb884ea2a8ef66
yosefkurniawan <jojo.maniack@gmail.com>
2014-03-29 03:03:27 +0700

    edit form paket pertanyaan

--- application/modules_core/soal/views/form_info.php
+++ application/modules_core/soal/views/form_info.php
@@ -1,7 +1,56 @@
-<h1>Informasi Paket</h1>
-<?php 
-echo '<pre>';
-print_r($info_paket);
-echo '</pre>';
-
- ?>
+<div class="page-header">
+	<h1>Pengaturan Paket Pertanyaan</h1>
+</div>
+<div class="panel colored">
+	<div class="panel-heading green-bg">
+		<h3 class="panel-title">Informasi Paket</h3>
+	</div>
+	<form role="form">
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Kode</label>
+						<div class="controls">
+							<input type="text" class="form-control">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Tahun Ajaran</label>
+						<div class="controls">
+							<input type="text" class="form-control">
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Semester</label>
+						<div class="controls">
+							<input type="text" class="form-control">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Status</label>
+						<div class="controls">
+							<input type="text" class="form-control">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</form>
+	<div class="panel-footer">
+		<div class="form-group">
+			<a href="#" class="btn btn-med blue-bg">Submit Details</a> 
+		</div>
+	</div>
+</div>
+ 
--- application/modules_core/soal/views/form_pertanyaan.php
+++ application/modules_core/soal/views/form_pertanyaan.php
@@ -1,39 +1,46 @@
-<h1>Pertanyaan</h1>
+<div class="panel colored box-form-pertanyaan">
+	<div class="panel-heading green-bg">
+		<h3 class="panel-title">Pertanyaan</h3>
+	</div>
+	<div class="panel-body">
+		<ul id="number">
+			<?php for ($i=1; $i <=12 ; $i++) { 
+				echo "<li><span>$i</span></li>";
+			} ?>
+		</ul>
 
-<ul id="number">
-	<?php for ($i=1; $i <=12 ; $i++) { 
-		echo "<li>$i</li>";
-	} ?>
-</ul>
-
-<ul class="sortable" id="list-pertanyaan">
-<?php for ($i=1; $i <=12 ; $i++) {?> 
-	<li id="pertanyaan<?php echo $i ?>">
-		<form role="form">
-			<div class="row">
-				<div class="col-md-3">
-					<div class="form-group">
-						<label>Aspek</label>
-						<select name="aspek" class="form-control">
-							<?php foreach ($list_aspek as $aspek) {
-								echo "<option value='".$aspek['id_aspek']."'>".$aspek['keterangan']."</option>";
-							} ?>
-						</select>
+		<ul class="sortable" id="list-pertanyaan">
+		<?php for ($i=1; $i <=12 ; $i++) {?> 
+			<li id="pertanyaan<?php echo $i ?>">
+				<form role="form">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="form-group">
+								<label>Aspek</label>
+								<select name="aspek" class="form-control">
+									<?php foreach ($list_aspek as $aspek) {
+										echo "<option value='".$aspek['id_aspek']."'>".$aspek['keterangan']."</option>";
+									} ?>
+								</select>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<div class="form-group">
+								<label>Pertanyaan</label>
+								<textarea class="form-control" name="isi_pertanyaan"></textarea>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-9">
-					<div class="form-group">
-						<label>Pertanyaan</label>
-						<textarea class="form-control" name="isi_pertanyaan"></textarea>
-					</div>
-				</div>
-			</div>
-		</form>
-	</li>
-<?php } ?>
-</ul>
+				</form>
+			</li>
+		<?php } ?>
+		</ul>
 
-<a href="javascript:void(0)" class="blue-bg btn btn-lg showcase-btn" id="save-paket">Simpan</a>
-<div id="save-paket-loading">
-	<img src="http://stuff.dan.cx/common/spinner.gif" alt="Loading..." title="Loading..." />Loading...
+	</div>
+	<div class="panel-footer">
+		<a href="javascript:void(0)" class="blue-bg btn" id="save-paket">Simpan</a>
+		<div id="save-paket-loading">
+			<img src="http://stuff.dan.cx/common/spinner.gif" alt="Loading..." title="Loading..." />Loading...
+		</div>
+	</div>
 </div>
