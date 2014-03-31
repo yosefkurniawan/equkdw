17759622fe30c9fc5e2d82e34b53f3ada4d56534
eska90 <sliding_dash@yahoo.co.id>
2014-03-31 14:19:41 +0700

    manajemen user

---
+++ application/modules_core/manajemen/views/ubah.php
@@ -0,0 +1,32 @@
+<div class="page-header">
+	<h1>Ubah Role User</h1>
+</div>
+<div class="panel colored">
+	<?php echo $this->session->userdata('message'); ?>
+	<form method="POST" action="<?php echo base_url(); ?>manajemen/user/ubah_proses">
+		<input type="hidden" name="user" value="<?php echo $this->session->userdata('username'); ?>">
+		<input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="form-group">
+						<label>Role</label>
+						<div class="controls">
+							<select name="role" class="form-control">
+								<option value="">-- PILIH --</option>
+								<option value="super admin" <?php $user['role'] == 'super admin' ? print "selected='selected'" : '' ?>>SUPER ADMIN</option>
+								<option value="admin" <?php $user['role'] == 'admin' ? print "selected='selected'" : '' ?>>ADMIN</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel-footer">
+			<div class="form-group">
+				<input type="submit" name="submit" class="btn btn-med blue-bg" value="simpan" />
+			</div>
+		</div>
+	</form>
+</div>
+ 
