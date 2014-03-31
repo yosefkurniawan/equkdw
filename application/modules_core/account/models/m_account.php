4847633a11e83337e01188b51e11009cb218ad58
eska90 <sliding_dash@yahoo.co.id>
2014-03-31 17:43:28 +0700

    edit password done

--- application/modules_core/account/models/m_account.php
+++ application/modules_core/account/models/m_account.php
@@ -21,6 +21,25 @@ class m_account extends CI_Model
 		$sql = "UPDATE eva_user SET password=? WHERE email=?";
 		return $this->db->query($sql, $params);
 	}
+
+	// validate password lama
+	function validate_password($params) {
+        $sql = "SELECT *
+		FROM eva_user
+		WHERE username = ? AND password = ?";
+        $query = $this->db->query($sql, $params);
+        if ($query->num_rows() > 0) {
+            return $query->row_array();
+        } else {
+            return false;
+        }
+	}
+
+	// edit password
+	function edit_password($params) {
+		$sql = "UPDATE eva_user SET password=? WHERE username=?";
+		return $this->db->query($sql, $params);
+	}
 }
 
 
